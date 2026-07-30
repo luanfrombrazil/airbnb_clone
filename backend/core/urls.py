726521f8aa49c24fb7f2579c46b67e8f1cd1fd30
luanfrombrazil <luanlana890@gmail.com>
2026-07-30 14:01:03 -0300

    Modificação das rotas principais para agrupar tudo sob /api/

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/users/', include('users.urls')),
-    path('api/listings/', include('listings.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path("admin/", admin.site.urls),
+    path("api/", include("users.urls")),
+    path("api/", include("listings.urls")),
+]
