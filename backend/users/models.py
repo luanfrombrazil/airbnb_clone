2a6225ecdabd91a91e7513cbf70369e458d4d5e1
luanfrombrazil <luanlana890@gmail.com>
2026-07-30 14:01:14 -0300

    Criação do model User com o campo is_host, separando anfitrião de hóspede

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
-    is_host = models.BooleanField(default=False)
-    is_guest = models.BooleanField(default=False)
+    """Usuário da plataforma: hóspede por padrão, anfitrião se is_host=True."""
+
+    is_host = models.BooleanField("é anfitrião?", default=False)
+
+    def __str__(self):
+        return self.username
