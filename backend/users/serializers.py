f9d83680ca70c14e8d713860a44b8ee4998a2b0f
luanfrombrazil <luanlana890@gmail.com>
2026-07-30 14:01:14 -0300

    Criação dos serializers de usuário e de cadastro, usando create_user para gerar o hash da senha

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,14 +1,22 @@
-from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from rest_framework import serializers
 
 User = get_user_model()
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password', 'is_host', 'is_guest']
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ["id", "username", "email", "first_name", "is_host"]
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=6)
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "first_name", "is_host", "password"]
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-        return user
+        # create_user já faz o hash da senha (o create() comum não faria).
+        return User.objects.create_user(**validated_data)
